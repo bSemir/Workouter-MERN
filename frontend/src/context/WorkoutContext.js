abdc9bdb696c3d5def672191fc39b4323991cf1d
Semir <sblekic1@etf.unsa.ba>
2022-09-11 01:26:39 +0200

    deleting data

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -6,11 +6,15 @@ export const workoutsReducer = (state, action) => {
     switch (action.type) {
         case 'SET_WORKOUTS':
             return {
-                workouts: action.payload
+                workouts: action.payload //action.payload is whatever server returned in backend(json)
             }
         case 'CREATE_WORKOUT':
             return {
                 workouts: [action.payload, ...state.workouts]
+            }
+        case 'DELETE_WORKOUT':
+            return {
+                workouts: state.workouts.filter((w) => w._id !== action.payload._id) //keep workouts in array if they're not equal
             }
         default:
             return state
