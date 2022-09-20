2f5d831d2a72c59148390a5d25c333a29a9a39af
Semir <sblekic1@etf.unsa.ba>
2022-09-21 00:09:48 +0200

    useLogin, useLogout hooks and little bug fixed

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const Navbar = () => {
+
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+  const handleClick = () => {
+    logout();
+  }
 
   return (
     <header>
@@ -9,10 +17,17 @@ const Navbar = () => {
           <h1>Workouter</h1>
         </Link>
         <nav>
-          <div>
-            <Link to="/login"> Login </Link>
-            <Link to="/signup"> Signup </Link>
-          </div>
+          {user && ( //only if we have value in user, output this!
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Log out</button>
+            </div>)}
+          {!user &&
+            (<div>
+              <Link to="/login"> Login </Link>
+              <Link to="/signup"> Signup </Link>
+            </div>
+            )}
         </nav>
       </div>
     </header>
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 export const AuthContext = createContext();
 
@@ -23,6 +23,14 @@ export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, {
         user: null
     });
+    //when the aplication loads and AuthContextProvider component renders, we are running useEffect just once to get user from localstorage
+    //because if we refresh the page, react thinks we are not logged in 
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'));
+
+        if (user)
+            dispatch({ type: 'LOGIN', payload: user });
+    }, [])
 
     console.log('AuthContext state: ', state);
 
