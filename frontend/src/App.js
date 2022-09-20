98f73442ca0cde068287b4c5aaddeb08ad000e56
Semir <sblekic1@etf.unsa.ba>
2022-09-20 21:27:46 +0200

    Signup && Login forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './components/Navbar';
 import WorkoutEdit from "./components/WorkoutEdit"
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -20,6 +22,14 @@ function App() {
             <Route path="/api/workouts/:id"
               element={<WorkoutEdit />}>
             </Route>
+            <Route
+              path='/login'
+              element={<Login />}
+            />
+            <Route
+              path='/signup'
+              element={<Signup />}
+            />
           </Routes>
         </div>
 
